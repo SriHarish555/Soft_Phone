eee305e2d386ec9e386b3bf72d55750f7336018e
Sri Harish <sriharish7635@gmail.com>
2024-02-28 23:37:45 +0530

    initial commit

---
+++ app/src/main/java/com/sece_connect/helloworld/MainActivity.kt
@@ -0,0 +1,148 @@
+package com.sece_connect.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import org.linphone.core.*
+import org.linphone.core.tools.Log
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var core: Core
+
+    private val coreListener = object: CoreListenerStub() {
+        override fun onAccountRegistrationStateChanged(core: Core, account: Account, state: RegistrationState, message: String) {
+            // If account has been configured correctly, we will go through Progress and Ok states
+            // Otherwise, we will be Failed.
+            findViewById<TextView>(R.id.registration_status).text = message
+
+            if (state == RegistrationState.Failed || state == RegistrationState.Cleared) {
+                findViewById<Button>(R.id.connect).isEnabled = true
+            } else if (state == RegistrationState.Ok) {
+                findViewById<Button>(R.id.disconnect).isEnabled = true
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val factory = Factory.instance()
+        factory.setDebugMode(true,"hello Linephone")
+        core=factory.createCore(null,null,this)
+
+        findViewById<Button>(R.id.connect).setOnClickListener {
+            login()
+            it.isEnabled = false
+        }
+
+        findViewById<Button>(R.id.disconnect).setOnClickListener {
+            unregister()
+            it.isEnabled = false
+        }
+
+        findViewById<Button>(R.id.delete).setOnClickListener {
+            delete()
+            it.isEnabled = false
+        }
+        val coreVersion = findViewById<TextView>(R.id.core_version)
+        coreVersion.text = core.version
+    }
+
+    private fun login() {
+        val username = findViewById<EditText>(R.id.username).text.toString()
+        val password = findViewById<EditText>(R.id.password).text.toString()
+        val domain = findViewById<EditText>(R.id.domain).text.toString()
+        // Get the transport protocol to use.
+        // TLS is strongly recommended
+        // Only use UDP if you don't have the choice
+        val transportType = when (findViewById<RadioGroup>(R.id.transport).checkedRadioButtonId) {
+            R.id.udp -> TransportType.Udp
+            R.id.tcp -> TransportType.Tcp
+            else -> TransportType.Tls
+        }
+
+        // To configure a SIP account, we need an Account object and an AuthInfo object
+        // The first one is how to connect to the proxy server, the second one stores the credentials
+
+        // The auth info can be created from the Factory as it's only a data class
+        // userID is set to null as it's the same as the username in our case
+        // ha1 is set to null as we are using the clear text password. Upon first register, the hash will be computed automatically.
+        // The realm will be determined automatically from the first register, as well as the algorithm
+        val authInfo = Factory.instance().createAuthInfo(username, null, password, null, null, domain, null)
+
+        // Account object replaces deprecated ProxyConfig object
+        // Account object is configured through an AccountParams object that we can obtain from the Core
+        val accountParams = core.createAccountParams()
+
+        // A SIP account is identified by an identity address that we can construct from the username and domain
+        val identity = Factory.instance().createAddress("sip:$username@$domain")
+        accountParams.identityAddress = identity
+
+        // We also need to configure where the proxy server is located
+        val address = Factory.instance().createAddress("sip:$domain")
+        // We use the Address object to easily set the transport protocol
+        address?.transport = transportType
+        accountParams.serverAddress = address
+        // And we ensure the account will start the registration process
+//        accountParams.registerEnabled = true
+
+        // Now that our AccountParams is configured, we can create the Account object
+        val account = core.createAccount(accountParams)
+
+        // Now let's add our objects to the Core
+        core.addAuthInfo(authInfo)
+        core.addAccount(account)
+
+        // Also set the newly added account as default
+        core.defaultAccount = account
+
+        // Allow account to be removed
+        findViewById<Button>(R.id.delete).isEnabled = true
+
+        // To be notified of the connection status of our account, we need to add the listener to the Core
+        core.addListener(coreListener)
+        // We can also register a callback on the Account object
+        account.addListener { _, state, message ->
+            // There is a Log helper in org.linphone.core.tools package
+            Log.i("[Account] Registration state changed: $state, $message")
+        }
+
+        // Finally we need the Core to be started for the registration to happen (it could have been started before)
+        core.start()
+    }
+
+    private fun unregister() {
+        // Here we will disable the registration of our Account
+        val account = core.defaultAccount
+        account ?: return
+
+        val params = account.params
+        // Returned params object is const, so to make changes we first need to clone it
+        val clonedParams = params.clone()
+
+        // Now let's make our changes
+//        clonedParams.registerEnabled = false
+
+        // And apply them
+        account.params = clonedParams
+    }
+
+    private fun delete() {
+        // To completely remove an Account
+        val account = core.defaultAccount
+        account ?: return
+        core.removeAccount(account)
+
+        // To remove all accounts use
+        core.clearAccounts()
+
+        // Same for auth info
+        core.clearAllAuthInfo()
+    }
+}
